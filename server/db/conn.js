65ccaf022c625d4f7d35b5ea80551abc2a35334b
Charliesolo <charliesolomon99@googlemail.com>
2024-11-18 10:05:08 +0000

    messed around with mongodb

--- server/db/conn.js
+++ server/db/conn.js
@@ -1,12 +1,16 @@
 
 const { MongoClient } = require("mongodb");
-const connectionString = process.env.ATLAS_URI;
-const client = new MongoClient(connectionString, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+const connectionString = process.env.ATLAS_URI || "";
+const client = new MongoClient(connectionString);
 
-let dbConnection;
+let connection;
+try {
+  connection = await client.connect();
+} catch(e) {
+  console.error(e);
+}
+let db = connection.db("sample_training");
+export default db;
 
 module.exports = {
   connectToServer: function (callback) {
